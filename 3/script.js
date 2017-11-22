425496b22146b27762a778da8c46d86e8cba333b
saburday <yteslenko@yahoo.com>
2017-11-22 23:38:04 +0200

    4.1

--- 3/script.js
+++ 3/script.js
@@ -1,9 +1,9 @@
 
  function fun1() {
-   var rtl = document.getElementById('rtl');
-   var rtr = document.getElementById('rtr');
-   var rbr = document.getElementById('rbr');
-   var rbl = document.getElementById('rbl');
+   var rtl = document.getElementById('rtl').value;
+   var rtr = document.getElementById('rtr').value;
+   var rbr = document.getElementById('rbr').value;
+   var rbl = document.getElementById('rbl').value;
    var ttl = document.getElementById('ttl');
    var ttr = document.getElementById('ttr');
    var tbr = document.getElementById('tbr');
